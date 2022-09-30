22885e5fb75ec75a39aea12700dce93052d7df65
yeruiyi <yeruiyi0@gmail.com>
2022-10-01 04:07:56 +0800

    Cluster marker with popup

--- src/Leaflet/Cluster/Cluster.tsx
+++ src/Leaflet/Cluster/Cluster.tsx
@@ -1,6 +1,6 @@
 import L, { LatLng, GeoJSON, latLng } from 'leaflet';
 import MarkerClusterGroup from 'react-leaflet-cluster'
-import { Marker } from 'react-leaflet';
+import { Marker, Popup } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 import useEarthquakesFetcher from '../Earthquakes/hooks';
 import { useSelector } from 'react-redux';
@@ -8,7 +8,14 @@ import { RooState } from '../../store';
 import { onEachFeature } from '../Earthquakes/utils';
 import { FeatureProps } from '../Earthquakes/models';
 import Spinner from '../../Spinner';
-type AddressPoint = Array<[number, number, number]>;
+import styled from 'styled-components';
+
+const timeConverter = (time: number, offset: number): string => {
+    const d = new Date(time);
+    const utc = d.getTime() + d.getTimezoneOffset() * 60000; // This converts to UTC 00:00
+    const nd = new Date(utc + 3600000 * offset);
+    return nd.toLocaleString();
+};
 
 const customIcon = new L.Icon({
     iconUrl: require("../images/location.svg").default,
@@ -25,7 +32,7 @@ export default function Cluster() {
     geojson = L.geoJSON(earthquakes.features, {
         onEachFeature,
         pointToLayer: (feature: FeatureProps, latlng: LatLng) => {
-          data.push(feature.geometry.coordinates)
+          data.push([feature.geometry.coordinates, feature.properties.title, feature.properties.place, feature.properties.time, feature.properties.mag])
           return L.marker(latlng);
         }
     });
@@ -33,23 +40,33 @@ export default function Cluster() {
     if (!loading && clusterEnabled){
         return (
             <MarkerClusterGroup chunkedLoading>
-                {(data as AddressPoint).map((address, index) => (
+                {data.map((address, index) => (
                     <Marker
                         icon={customIcon}
                         key={index}
-                        position={[address[1], address[0]]}
-                    ></Marker>
+                        position={[Number(address[0][1]), Number(address[0][0])]}
+                    >
+                        <Popup>
+                            <PopupHeader> {address[1]} </PopupHeader>
+                            <b>Place</b>: {address[2]} <br/>
+                            <b>Time (GMC+3)</b>: {timeConverter(address[3], 3)} <br/>
+                            <b>Lat</b>: {address[0][1]} &nbsp;
+                            <b>Lon</b>: {address[0][0]} <br/>
+                            <b>Depth</b>: {address[0][2]} km <br/>
+                            <b>Magnitude</b>: {address[4]} Richter <br></br>
+                        </Popup>
+                    </Marker>
                 ))}
             </MarkerClusterGroup>
         )
     } else if (loading) {
-        return (
-            <Spinner />
-        )
+        return ( <Spinner /> )
     } else {
-        return(
-            <></>
-        )
+        return ( <></> )
     }
   }
-  
+
+  const PopupHeader = styled.h3`
+    font-size: 1.17em;
+    font-weight: bold;
+`;
