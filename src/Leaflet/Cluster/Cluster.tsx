0b2a60716f032838d062339fb8c5239ae012605a
yeruiyi <yeruiyi0@gmail.com>
2022-10-02 04:05:49 +0800

    Refine cluster code

--- src/Leaflet/Cluster/Cluster.tsx
+++ src/Leaflet/Cluster/Cluster.tsx
@@ -9,7 +9,7 @@ import { onEachFeature } from '../Earthquakes/utils';
 import { FeatureProps } from '../Earthquakes/models';
 import Spinner from '../../Spinner';
 import styled from 'styled-components';
-
+import { textProps } from '../../Navbar/TextData/models';
 const timeConverter = (time: number, offset: number): string => {
     const d = new Date(time);
     const utc = d.getTime() + d.getTimezoneOffset() * 60000; // This converts to UTC 00:00
@@ -22,41 +22,42 @@ const customIcon = new L.Icon({
     iconSize: new L.Point(40, 47)
 });
 
-let geojson: GeoJSON;
+// let geojson: GeoJSON;
 export default function Cluster() {
     const { startTime, endTime, longitude, latitude, maxradius, orderby, minlongitude, minlatitude, maxlongitude, maxlatitude, countEnabled,clusterEnabled,minMag, maxMag } = useSelector(({ navbar }: RooState) => navbar);
     const [earthquakes, loading, eqCount] = useEarthquakesFetcher(startTime, endTime, longitude, latitude, maxradius, orderby, minlongitude, minlatitude, maxlongitude, maxlatitude, countEnabled, minMag, maxMag);
 
-
-    let data: any[] = [];
-    geojson = L.geoJSON(earthquakes.features, {
-        onEachFeature,
-        pointToLayer: (feature: FeatureProps, latlng: LatLng) => {
-          data.push([feature.geometry.coordinates, feature.properties.title, feature.properties.place, feature.properties.time, feature.properties.mag])
-          return L.marker(latlng);
-        }
-    });
-
     if (!loading && clusterEnabled){
         return (
             <MarkerClusterGroup chunkedLoading>
-                {data.map((address, index) => (
-                    <Marker
-                        icon={customIcon}
-                        key={index}
-                        position={[Number(address[0][1]), Number(address[0][0])]}
-                    >
-                        <Popup>
-                            <PopupHeader> {address[1]} </PopupHeader>
-                            <b>Place</b>: {address[2]} <br/>
-                            <b>Time (GMC+3)</b>: {timeConverter(address[3], 3)} <br/>
-                            <b>Lat</b>: {address[0][1]} &nbsp;
-                            <b>Lon</b>: {address[0][0]} <br/>
-                            <b>Depth</b>: {address[0][2]} km <br/>
-                            <b>Magnitude</b>: {address[4]} Richter <br></br>
-                        </Popup>
-                    </Marker>
-                ))}
+                {earthquakes.features.map((feature: textProps) => {
+                    const lat = feature.geometry.coordinates[1]
+                    const lon = feature.geometry.coordinates[0]
+                    var mag = feature.properties.mag
+                    if(feature.properties.mag){
+                        mag = feature.properties.mag
+                    } else {
+                        mag = 0
+                    }
+                    return (
+                        <Marker
+                            icon={customIcon}
+                            key={feature.id}
+                            position={[Number(lat), Number(lon)]}
+                            >
+                            <Popup>
+                                <PopupHeader> {feature.properties.title} </PopupHeader>
+                                <b>Place</b>: {feature.properties.place} <br/>
+                                <b>Time (GMC+3)</b>: {timeConverter(feature.properties.time, 3)} <br/>
+                                <b>Lat</b>: {lat} &nbsp;
+                                <b>Lon</b>: {lon} <br/>
+                                <b>Depth</b>: {feature.geometry.coordinates[2]} km <br/>
+                                <b>Magnitude</b>: {mag} Richter <br/>
+                                <b>Details</b>: <a href={feature.properties.url}>click here to see more details</a>
+                            </Popup>
+                        </Marker>
+                    );
+                })}
             </MarkerClusterGroup>
         )
     } else if (loading) {
